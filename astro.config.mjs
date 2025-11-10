605d54843352dc93744f00c1d3f473c3e5330d6d
David Diaz <it@hairmedicalrestoration.com>
2025-11-10 15:29:33 -0700

    Versión base para hairless

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  //output: 'static', // 👈 MUY IMPORTANTE: le dice a Astro que genere archivos HTML estáticos
+  site: 'https://winhairtj.com', // cambia por tu dominio real
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
